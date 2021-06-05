ecba468115d2a1d049f425ee7cf6deb3f1147d51
Vadim Cpp <vadim.cpp@gmail.com>
2021-06-05 18:55:29 +0300

    Refactor MainScreen to use Hotel

--- project/src/components/hotel/hotel.jsx
+++ project/src/components/hotel/hotel.jsx
@@ -1,15 +1,43 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 function Hotel({preview, price, rating, title, type}) {
   return (
-    <div className="hotel">
-      <img className="hotel__preview" src={preview} alt="preview" width="260" height="200"/>
-      <span className="hotel__price"> {price} € / night</span>
-      <span className="hotel__rating">{rating}</span>
-      <span className="hotel__title">{title}</span>
-      <span className="hotel__type">{type}</span>
-    </div>
+    <article className="cities__place-card place-card">
+      <div className="place-card__mark">
+        <span>Premium</span>
+      </div>
+      <div className="cities__image-wrapper place-card__image-wrapper">
+        <Link to="/">
+          <img className="place-card__image" src={preview} width="260" height="200" alt="Place image" />
+        </Link>
+      </div>
+      <div className="place-card__info">
+        <div className="place-card__price-wrapper">
+          <div className="place-card__price">
+            <b className="place-card__price-value">&euro;{price}</b>
+            <span className="place-card__price-text">&#47;&nbsp;night</span>
+          </div>
+          <button className="place-card__bookmark-button button" type="button">
+            <svg className="place-card__bookmark-icon" width="18" height="19">
+              <use xlinkHref="#icon-bookmark"></use>
+            </svg>
+            <span className="visually-hidden">To bookmarks</span>
+          </button>
+        </div>
+        <div className="place-card__rating rating">
+          <div className="place-card__stars rating__stars">
+            <span style={{ width: '100%' }}></span>
+            <span className="visually-hidden">Rating {rating}</span>
+          </div>
+        </div>
+        <h2 className="place-card__name">
+          <Link to="/">{title}</Link>
+        </h2>
+        <p className="place-card__type">{type}</p>
+      </div>
+    </article>
   );
 }
 
