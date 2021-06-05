6d2a2c89554023c123da0af493aecb5c3310570c
Vadim Cpp <vadim.cpp@gmail.com>
2021-06-05 16:33:15 +0300

    Add routes

--- project/src/components/app/app.jsx
+++ project/src/components/app/app.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import MainScreen from '../main-screen/main-screen';
+import LoginScreen from '../login-screen/login-screen';
+import FavoritesScreen from '../favorites-screen/favorites-screen';
+import RoomScreen from '../room-screen/room-screen';
+import NotFoundScreen from '../not-found-screen/not-found-screen';
 
 function App({places, hotels}) {
-  return <MainScreen places={places} hotels={hotels}/>;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact>
+          <MainScreen places={places} hotels={hotels}/>
+        </Route>
+        <Route path="/login" exact components={LoginScreen} />
+        <Route path="/favorites" exact component={FavoritesScreen} />
+        <Route path="/offer/:id" exact component={RoomScreen} />
+        <Route component={NotFoundScreen} />
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 App.propTypes = {
