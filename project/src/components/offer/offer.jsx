e106fe496618b609c42ff77fa6084d7654595b11
Vadim Cpp <vadim.cpp@gmail.com>
2021-06-14 18:55:03 +0300

    Display bookmark

--- project/src/components/offer/offer.jsx
+++ project/src/components/offer/offer.jsx
@@ -35,11 +35,11 @@ function Offer({ offer, onMouseEnter, onMouseLeave, isActive, fromFavoriteScreen
             <b className="place-card__price-value">&euro;{price}</b>
             <span className="place-card__price-text">&#47;&nbsp;night</span>
           </div>
-          <button className="place-card__bookmark-button button" type="button">
+          <button className={`place-card__bookmark-button ${offer.isFavorite ? 'place-card__bookmark-button--active' : ''} button`} type="button">
             <svg className="place-card__bookmark-icon" width="18" height="19">
               <use xlinkHref="#icon-bookmark"></use>
             </svg>
-            <span className="visually-hidden">To bookmarks</span>
+            <span className="visually-hidden">${offer.isFavorite ? 'From' : 'To '} bookmarks</span>
           </button>
         </div>
         <div className="place-card__rating rating">
