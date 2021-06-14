33d160c8e332b102a2ff2a516af6cd7a32d7b75b
Vadim Cpp <vadim.cpp@gmail.com>
2021-06-14 18:27:33 +0300

    Add CommentStar component

--- project/src/components/comment-form/comment-form.jsx
+++ project/src/components/comment-form/comment-form.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import CommentStar from '../comment-star/comment-start';
 
 function ComponentForm() {
   const [comment, setComment] = useState('');
@@ -26,40 +27,13 @@ function ComponentForm() {
     <form className="reviews__form form" onSubmit={handleSubmit}>
       <label className="reviews__label form__label" htmlFor="review">Your review</label>
       <div className="reviews__rating-form form__rating">
-        <input className="form__rating-input visually-hidden" name="rating" value="5" id="5-stars" type="radio" onChange={handleRatingChange} />
-        <label htmlFor="5-stars" className="reviews__rating-label form__rating-label" title="perfect">
-          <svg className="form__star-image" width="37" height="33">
-            <use xlinkHref="#icon-star"></use>
-          </svg>
-        </label>
-
-        <input className="form__rating-input visually-hidden" name="rating" value="4" id="4-stars" type="radio" onChange={handleRatingChange} />
-        <label htmlFor="4-stars" className="reviews__rating-label form__rating-label" title="good">
-          <svg className="form__star-image" width="37" height="33">
-            <use xlinkHref="#icon-star"></use>
-          </svg>
-        </label>
-
-        <input className="form__rating-input visually-hidden" name="rating" value="3" id="3-stars" type="radio" onChange={handleRatingChange} />
-        <label htmlFor="3-stars" className="reviews__rating-label form__rating-label" title="not bad">
-          <svg className="form__star-image" width="37" height="33">
-            <use xlinkHref="#icon-star"></use>
-          </svg>
-        </label>
-
-        <input className="form__rating-input visually-hidden" name="rating" value="2" id="2-stars" type="radio" onChange={handleRatingChange} />
-        <label htmlFor="2-stars" className="reviews__rating-label form__rating-label" title="badly">
-          <svg className="form__star-image" width="37" height="33">
-            <use xlinkHref="#icon-star"></use>
-          </svg>
-        </label>
-
-        <input className="form__rating-input visually-hidden" name="rating" value="1" id="1-star" type="radio" onChange={handleRatingChange} />
-        <label htmlFor="1-star" className="reviews__rating-label form__rating-label" title="terribly">
-          <svg className="form__star-image" width="37" height="33">
-            <use xlinkHref="#icon-star"></use>
-          </svg>
-        </label>
+        {[
+          { stars: 5, title: 'perfect' },
+          { stars: 4, title: 'good' },
+          { stars: 3, title: 'not bad' },
+          { stars: 2, title: 'badly' },
+          { stars: 1, title: 'terribly' },
+        ].map(({ stars, title }) => <CommentStar key={stars} stars={stars} title={title} handleRatingChange={handleRatingChange} />)}
       </div>
       <textarea
         className="reviews__textarea form__textarea"
