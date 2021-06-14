20134c8708f5bf95ae85b5786eb40cdbe26734d9
Vadim Cpp <vadim.cpp@gmail.com>
2021-06-14 18:28:30 +0300

    Remove closed tag

--- project/src/components/comment-form/comment-form.jsx
+++ project/src/components/comment-form/comment-form.jsx
@@ -40,9 +40,7 @@ function ComponentForm() {
         placeholder="Tell how was your stay, what you like and what can be improved"
         value={comment}
         onChange={handleCommentChange}
-      >
-
-      </textarea>
+      />
       <div className="reviews__button-wrapper">
         <p className="reviews__help">
           To submit review please make sure to set <span className="reviews__star">rating</span> and describe your stay with at least <b className="reviews__text-amount">50 characters</b>.
