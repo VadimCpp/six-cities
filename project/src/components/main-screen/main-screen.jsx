ccdcc5b607f54cba2c6177ceadb5815b53acec2b
Vadim Cpp <vadim.cpp@gmail.com>
2021-06-15 20:44:41 +0300

    Add TODO

--- project/src/components/main-screen/main-screen.jsx
+++ project/src/components/main-screen/main-screen.jsx
@@ -54,12 +54,13 @@ function MainScreen({offers}) {
                     <use xlinkHref="#icon-arrow-select"></use>
                   </svg>
                 </span>
-                <ul className="places__options places__options--custom places__options--opened">
+                {/* TODO: реализовать */}
+                {/* <ul className="places__options places__options--custom places__options--opened">
                   <li className="places__option places__option--active" tabIndex="0">Popular</li>
                   <li className="places__option" tabIndex="0">Price: low to high</li>
                   <li className="places__option" tabIndex="0">Price: high to low</li>
                   <li className="places__option" tabIndex="0">Top rated first</li>
-                </ul>
+                </ul> */}
               </form>
               <Offers offers={sortedOffers}/>
             </section>
