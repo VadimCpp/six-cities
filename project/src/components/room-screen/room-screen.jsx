14e3c02af462c5c3254674bca80b44f73bd7b618
Vadim Cpp <vadim.cpp@gmail.com>
2021-06-20 13:06:03 +0300

    Create Review component

--- project/src/components/room-screen/room-screen.jsx
+++ project/src/components/room-screen/room-screen.jsx
@@ -6,6 +6,7 @@ import { RATING_TO_PERCENTS } from '../../const';
 import Header from '../header/header';
 import Footer from '../footer/footer';
 import CommentForm from '../comment-form/comment-form';
+import Review from '../review/review';
 
 function RoomScreen({offer}) {
   return (
@@ -98,26 +99,7 @@ function RoomScreen({offer}) {
                 <h2 className="reviews__title">Reviews &middot; <span className="reviews__amount">1</span></h2>
                 <ul className="reviews__list">
                   <li className="reviews__item">
-                    <div className="reviews__user user">
-                      <div className="reviews__avatar-wrapper user__avatar-wrapper">
-                        <img className="reviews__avatar user__avatar" src="img/avatar-max.jpg" width="54" height="54" alt="Reviews avatar" />
-                      </div>
-                      <span className="reviews__user-name">
-                        Max
-                      </span>
-                    </div>
-                    <div className="reviews__info">
-                      <div className="reviews__rating rating">
-                        <div className="reviews__stars rating__stars">
-                          <span style={{ width: '80%' }}></span>
-                          <span className="visually-hidden">Rating</span>
-                        </div>
-                      </div>
-                      <p className="reviews__text">
-                        A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam. The building is green and from 18th century.
-                      </p>
-                      <time className="reviews__time" dateTime="2019-04-24">April 2019</time>
-                    </div>
+                    <Review />
                   </li>
                 </ul>
                 {/* TODO: отображать только для авторизованных пользователей */}
