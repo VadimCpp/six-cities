3b6468ef958a95d54a97222e5500bafb2ccf36ed
Vadim Cpp <vadim.cpp@gmail.com>
2021-06-22 01:04:37 +0300

    Connect redux

--- project/src/index.js
+++ project/src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import App from './components/app/app';
 import offers from './mocks/offers.json';
 import comments from './mocks/comments.json';
+import { reducer } from './store/reducer';
+
+const store = createStore(reducer);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App offers={offers} comments={comments}/>
+    <Provider store={store}>
+      <App offers={offers} comments={comments}/>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'));
