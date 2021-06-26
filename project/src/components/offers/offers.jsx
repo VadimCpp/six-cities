c4bc21ec175ae27de49af5e17a49bed62d8ba10b
Vadim Cpp <vadim.cpp@gmail.com>
2021-06-26 18:36:28 +0300

    Remove useless state and effect

--- project/src/components/offers/offers.jsx
+++ project/src/components/offers/offers.jsx
@@ -1,13 +1,9 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import offersProp from '../../types/offers.prop';
 import Offer from '../offer/offer';
 
 function Offers({offers, placesListClass, placeCardClass = '', imageWrapperClass = '', onActiveOfferSet = () => {}}) {
-  const [activeOfferId, setActiveOfferId] = useState(0);
-
-  useEffect(() => onActiveOfferSet(activeOfferId), [ activeOfferId, onActiveOfferSet ]);
-
   return (
     <div className={`${placesListClass} places__list`}>
       {offers.map((offer) =>
@@ -15,8 +11,8 @@ function Offers({offers, placesListClass, placeCardClass = '', imageWrapperClass
           <Offer
             key={offer.id}
             offer={offer}
-            onMouseEnter={() => setActiveOfferId(offer.id)}
-            onMouseLeave={() => setActiveOfferId(0)}
+            onMouseEnter={() => onActiveOfferSet(offer.id)}
+            onMouseLeave={() => onActiveOfferSet(0)}
             placeCardClass={placeCardClass}
             imageWrapperClass={imageWrapperClass}
           />
