35fe5f072d4b31b4c30ad79dfe693462355885a4
Vadim Cpp <vadim.cpp@gmail.com>
2021-06-29 02:23:49 +0300

    Fix Browser router

--- project/src/components/app/app.jsx
+++ project/src/components/app/app.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { Switch, Route, Router as BrowserRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
+import browserHistory from '../../browser-history';
 import { AppRoute } from '../../const';
 import offersProp from '../../types/offers.prop';
 import commentsProp from '../../types/comments.prop';
@@ -13,7 +14,7 @@ import NotFoundScreen from '../not-found-screen/not-found-screen';
 
 function App({offers, comments}) {
   return (
-    <BrowserRouter>
+    <BrowserRouter history={browserHistory}>
       <Switch>
         <Route path={AppRoute.ROOT} exact component={MainScreen} />
         <Route path={AppRoute.LOGIN} exact component={LoginScreen} />
