0f9d08c4ebfdcbf9d87f40797be09b57fd56de65
Vadim Cpp <vadim.cpp@gmail.com>
2021-06-30 23:59:44 +0300

    Implement OfferAdapter

--- project/src/store/api-actions.js
+++ project/src/store/api-actions.js
@@ -1,51 +1,21 @@
 import { generatePath } from 'react-router-dom';
-import {ActionCreator} from './action';
-import {AuthorizationStatus, APIRoute, AppRoute} from '../const';
+import { ActionCreator } from './action';
+import { AuthorizationStatus, APIRoute, AppRoute } from '../const';
+import OfferAdapter from '../utils/offerAdapter';
 
 export const fetchOfferList = () => (dispatch, _getState, api) => (
   api.get(APIRoute.OFFERS)
-    .then(({data}) => {
-      //
-      // NOTE!
-      // С бэка приходят переменные в формате snake_case
-      // Поэтому происходить преобразование к camelCase
-      //
-      // * Вместо паттерна Адаптер
-      //
-      data.forEach((offer) => {
-        offer.preview = offer['preview_image'];
-        offer.maxAdults = offer['max_adults'];
-        offer.isPremium = offer['is_premium'];
-        offer.host.avatarUrl = offer['host']['avatar_url'];
-        offer.host.isPro = offer['host']['is_pro'];
-      });
-
-      dispatch(ActionCreator.loadOffers(data));
-    })
+    .then(({data}) => dispatch(ActionCreator.loadOffers(OfferAdapter.getOffers(data))))
 );
 
 export const fetchOfferData = (id) => (dispatch, _getState, api) => (
   api.get(generatePath(APIRoute.OFFER, { id }))
-    .then(({data}) => {
-
-      //
-      // NOTE!
-      // С бэка приходят переменные в формате snake_case
-      // Поэтому происходить преобразование к camelCase
-      //
-      // * Вместо паттерна Адаптер
-      //
-      data.preview = data['preview_image'];
-      data.maxAdults = data['max_adults'];
-      data.isPremium = data['is_premium'];
-      data.host.avatarUrl = data['host']['avatar_url'];
-      data.host.isPro = data['host']['is_pro'];
-
+    .then(({data}) =>
       dispatch(ActionCreator.setOfferData({
         id: data.id,
-        offer: data,
-      }));
-    })
+        offer: OfferAdapter.getOffer(data),
+      })),
+    )
     .catch(() => dispatch(ActionCreator.redirectToRoute(AppRoute.NOT_FOUND)))
 );
 
