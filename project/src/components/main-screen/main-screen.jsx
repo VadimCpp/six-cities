9cb173aca846b3f19d938bd485a1daa4267566d1
Vadim Cpp <vadim.cpp@gmail.com>
2021-07-07 15:29:13 +0300

    Add main-empty case

--- project/src/components/main-screen/main-screen.jsx
+++ project/src/components/main-screen/main-screen.jsx
@@ -39,35 +39,49 @@ function MainScreen() {
   return (
     <div className="page page--gray page--main">
       <Header />
-      {
-        isDataLoaded ? (
-          <main className="page__main page__main--index">
-            <h1 className="visually-hidden">Cities</h1>
-            <Cities />
-            <div className="cities">
-              <div className="cities__places-container container">
-                <section className="cities__places places">
-                  <h2 className="visually-hidden">Places</h2>
-                  <b className="places__found">{offersForCity.length} places to stay in {city}</b>
-                  <Sorting type={sortType} onSortingChange={setSortType}/>
-                  <Offers
-                    offers={offersForCity}
-                    placesListClass="cities__places-list"
-                    placeCardClass="cities__place-card"
-                    imageWrapperClass="cities__image-wrapper"
-                    onActiveOfferSet={setActiveOfferId}
-                  />
-                </section>
-                <div className="cities__right-section">
-                  <CitiesMap city={CITIES[city]} offers={offersForCity} className="cities__map" activeOfferId={activeOfferId} />
-                </div>
+      {isDataLoaded && offersForCity.length > 0 && (
+        <main className="page__main page__main--index">
+          <h1 className="visually-hidden">Cities</h1>
+          <Cities />
+          <div className="cities">
+            <div className="cities__places-container container">
+              <section className="cities__places places">
+                <h2 className="visually-hidden">Places</h2>
+                <b className="places__found">{offersForCity.length} places to stay in {city}</b>
+                <Sorting type={sortType} onSortingChange={setSortType}/>
+                <Offers
+                  offers={offersForCity}
+                  placesListClass="cities__places-list"
+                  placeCardClass="cities__place-card"
+                  imageWrapperClass="cities__image-wrapper"
+                  onActiveOfferSet={setActiveOfferId}
+                />
+              </section>
+              <div className="cities__right-section">
+                <CitiesMap city={CITIES[city]} offers={offersForCity} className="cities__map" activeOfferId={activeOfferId} />
               </div>
             </div>
-          </main>
-        ) : (
-          <Spinner />
-        )
-      }
+          </div>
+        </main>
+      )}
+      {isDataLoaded && !offersForCity.length && (
+        <main className="page__main page__main--index page__main--index-empty">
+          <h1 className="visually-hidden">Cities</h1>
+          <Cities />
+          <div className="cities">
+            <div className="cities__places-container cities__places-container--empty container">
+              <section className="cities__no-places">
+                <div className="cities__status-wrapper tabs__content">
+                  <b className="cities__status">No places to stay available</b>
+                  <p className="cities__status-description">We could not find any property available at the moment in {city}</p>
+                </div>
+              </section>
+              <div className="cities__right-section"></div>
+            </div>
+          </div>
+        </main>
+      )}
+      {!isDataLoaded && <Spinner />}
     </div>
   );
 }
