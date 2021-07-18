f24d82347033150130ae57fc45468398197fad28
Вадим Канинский <vadim.cpp@gmail.com>
2021-07-18 20:00:37 +0300

    Fix ESLint

--- project/src/index.js
+++ project/src/index.js
@@ -6,18 +6,19 @@ import {Router as BrowserRouter} from 'react-router-dom';
 import rootReducer from './store/root-reducer';
 import { requireAuthorization } from './store/action';
 import { redirect } from './store/middlewares/redirect';
-import {checkAuth, fetchOfferList} from './store/api-actions';
+import { checkAuth, fetchOfferList } from './store/api-actions';
 import App from './components/app/app';
 import { createAPI } from './services/api';
 import { AuthorizationStatus } from './const';
 import browserHistory from './browser-history';
 
-// TODO: исправить ESLint ошибку no-use-before-define
+let store = null;
+
 const api = createAPI(
   () => store.dispatch(requireAuthorization(AuthorizationStatus.NO_AUTH)),
 );
 
-const store = configureStore({
+store = configureStore({
   reducer: rootReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
